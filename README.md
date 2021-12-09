77e50bfe11a0a2a927981e0a0a85dc733ec65356
M Ahmed Mushtaq <mahmedmushtaq296@gmail.com>
2021-12-09 11:15:00 +0500

    basic project setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# nft-marketplace
+**Please ensure you have downloaded the following additionally **
+
+1. Ganache
+2. Truffle (global installation)
+3. Metamask.io (hooked up on the browser)
+
+**To run the development server on a local host scripts:** npm run start
+
+For truffle tests and console please consult the official Truffle documentaion for updates.
+
+
---
+++ src/contracts/ERC721.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract ERC721 {
+    /**
+    
+    building out the minting function
+        a- nft to point to an address
+        b- keep track of the token ids
+        c- keep track of token owner address to the token ids
+        d- keep track of how many token an owner address has
+        e- create an event that emit and transfer log - contract address
+        where it is being minted to.
+
+    
+     */
+}
